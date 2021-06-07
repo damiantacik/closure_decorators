3c0e48f2c19ed4ee2ec34a439ad1f92e7e976715
damtac <damiantacik@gmail.com>
2021-06-07 17:45:58 +0000

    more advanced examples of using decorator

---
+++ decorators_order.py
@@ -0,0 +1,19 @@
+def upper(fn):
+    def inner(name):
+        return fn(name).title()
+    return inner
+
+
+def reverse(fn):
+    def inner(name):
+        return fn(name)[::-1]
+    return inner
+
+
+@upper
+@reverse
+def hello(name):
+    return f"Hello {name}"
+
+
+print(hello('pawel'))
